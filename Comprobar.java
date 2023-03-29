fd17aa5c1437877968fe27697e5caf77da8e3aa5
AdriSaurXYZ <adrianaguilo@paucasesnovescifp.cat>
2023-03-29 19:52:57 +0200

    Gmail

--- Comprobar.java
+++ Comprobar.java
@@ -4,30 +4,7 @@ import java.util.Scanner;
 public class Comprobar {
 
 
-        // Base de datos de nombres de usuarios
-        private static final String[] nombresUsuarios = {"Carles_345", "AnaGomez-783", "Pepito_999", "Laura_222",
-        "Juanito-111", "Marta_444", "Pedro_777", "Sofia-666", "David_555", "Lucia888"};
-
-        // Método para comprobar el nombre de usuario
-        public static boolean compruebaNombre(String nombre) {
-        boolean valido = false;
-
-        // Comprobar que el nombre cumple las restricciones
-        if (nombre.matches("^[A-Z][a-z]+[-][0-9]{3}$") && nombre.length() <= 16) {
-        // Comprobar que el nombre no está en la base de datos
-        for (String n : nombresUsuarios) {
-            if (n.equalsIgnoreCase(nombre)) {
-                System.out.println("El nombre de usuario ya existe");
-                return false;
-            }
-        }
-        valido = true;
-        } else {
-        System.out.println("El nombre de usuario no cumple las restricciones");
-        }
-
-        return valido;
-        }
+        
     
     
         // Método para comprobar la contraseña
